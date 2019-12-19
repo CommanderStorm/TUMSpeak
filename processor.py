8c97d98adbcf5cc10d1e97f6093067e8dd587d27
Frank <frank@elsinga.de>
2019-12-20 00:00:00 +0100

    added Font integration
    Signed-off-by: Frank <frank@elsinga.de>

--- processor.py
+++ processor.py
@@ -2,6 +2,8 @@ import datetime
 import os
 import time
 
+import Font
+
 
 def generate_new_filepath() -> str:
     time.sleep(1)
@@ -12,9 +14,21 @@ def generate_new_filepath() -> str:
     return path
 
 
+def processLine(i: str, xHeight: int, yHeight: int) -> str:
+    out = ""
+    for character in i:
+        out += Font.generate_svg_code(character, xHeight, yHeight)
+        xHeight += Font.get_character_width(character)
+    return out
+
+
 def process(text: str):
     text = text.upper().strip()
     print("Processing:\n%s" % text)
     output_path = generate_new_filepath()
     with open(output_path, "w") as o:
-        o.write(text)
+        xHeight, yHeight = 0, 0
+        for i in text.split("\n"):
+            o.write(processLine(i, xHeight, yHeight))
+            xHeight = 0
+            yHeight += 200
