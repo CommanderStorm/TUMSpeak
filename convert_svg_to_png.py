8723fa059f5589191474fc8a0b9d605f5332bc83
Frank <frank@elsinga.de>
2019-12-21 00:20:04 +0100

    changed formatting
    Signed-off-by: Frank <frank@elsinga.de>

--- convert_svg_to_png.py
+++ convert_svg_to_png.py
@@ -1,16 +1,16 @@
 def rasterise(source: str, target: str):
     print("""
-CONVERTING SVG at %s
-                   |   |         
-                   |   |         
-                   |   |         
-                   |   |         
-                   |   |         
-                 __!   !__,      
-                 \       / \O    
-                  \     / \/|    
-                   \   /    |    
-                    \ /    / \      
-                     Y   _/  _\     
-                to %s
+CONVERTING SVG at   %s
+                   |   |
+                   |   |
+                   |   |
+                   |   |
+                   |   |
+                 __!   !__,
+                 \       / \O
+                  \     / \/|
+                   \   /    |
+                    \ /    / \ 
+                     Y   _/  _\ 
+        to PNG at   %s
     """ % (source, target))
