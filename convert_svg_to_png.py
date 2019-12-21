57f6c86b7d2300953341300719e92e3c9727093b
Frank <frank@elsinga.de>
2019-12-21 11:05:18 +0100

    added partial png support
    Signed-off-by: Frank <frank@elsinga.de>

--- convert_svg_to_png.py
+++ convert_svg_to_png.py
@@ -1,3 +1,8 @@
+import wand.color
+import wand.image
+from wand.api import library
+
+
 def rasterise(source: str, target: str):
     print("""
 CONVERTING SVG at   %s
@@ -14,3 +19,14 @@ CONVERTING SVG at   %s
                      Y   _/  _\ 
         to PNG at   %s
     """ % (source, target))
+    with wand.image.Image() as image:
+        with wand.color.Color('transparent') as background_color:
+            library.MagickSetBackgroundColor(image.wand,
+                                             background_color.resource)
+        with open(source, "r")as sourceFile:
+            image.read(blob=sourceFile.read(), format="svg")
+
+        png_image = image.make_blob("png32")
+
+    with open(target, "wb") as out:
+        out.write(png_image)
